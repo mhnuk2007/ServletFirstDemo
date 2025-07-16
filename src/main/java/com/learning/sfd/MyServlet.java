762bc626c1d2cf70fad0af3b84de080916ab4760
Mohan Lal <mhnuk2007@gmail.com>
2025-07-17 01:58:08 +0500

    Updated MyServlet.java to send HTML response to browser

--- src/main/java/com/learning/sfd/MyServlet.java
+++ src/main/java/com/learning/sfd/MyServlet.java
@@ -1,20 +1,27 @@
 package com.learning.sfd;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-public class MyServlet extends HttpServlet{
-	@Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		//backend code
-		System.out.println("In doGet() method");
-	}
-	
-	
-	
+public class MyServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
 
+        // Set the content type
+        response.setContentType("text/html");
+
+        // Send response to browser
+        PrintWriter out = response.getWriter();
+        out.println("<h1>In doGet() method for browser</h1>");
+        out.close();
+
+        // Log to console
+        System.out.println("In doGet() method");
+    }
 }
